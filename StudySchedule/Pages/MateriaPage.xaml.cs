fbd1afc586a1774a5f0ce68bc6b6cb666f2fcf3b
marcus vogado <marcus_620@hotmail.com>
2023-05-11 01:02:49 -0300

    Teste, Lista de matérias

--- StudySchedule/Pages/MateriaPage.xaml.cs
+++ StudySchedule/Pages/MateriaPage.xaml.cs
@@ -1,9 +1,31 @@
+using MigrationLibrary.Models;
+
 namespace StudySchedule.Pages;
 
 public partial class MateriaPage : ContentPage
 {
-	public MateriaPage()
-	{
-		InitializeComponent();
-	}
+    List<Materia> listMateria = new List<Materia> {
+        new Materia {NomeMateria="Portugues",Id=1,BackGroundColor= "1" },
+        new Materia {NomeMateria="Matemática",Id=1,BackGroundColor= "Green" },
+        new Materia {NomeMateria="Programação Orientada a Objetos",Id=1,BackGroundColor= "Red" },
+         new Materia {NomeMateria="Programação Orientada a Objetos",Id=1,BackGroundColor= "Red" },
+          new Materia {NomeMateria="Programação Orientada a Objetos",Id=1,BackGroundColor= "Red" },
+           new Materia {NomeMateria="Programação Orientada a Objetos",Id=1,BackGroundColor= "Red" },
+            new Materia {NomeMateria="Programação Orientada a Objetos",Id=1,BackGroundColor= "Red" },
+             new Materia {NomeMateria="Programação Orientada a Objetos",Id=1,BackGroundColor= "Red" },
+              new Materia {NomeMateria="Programação Orientada a Objetos",Id=1,BackGroundColor= "Red" },
+               new Materia {NomeMateria="Programação Orientada a Objetos",Id=1,BackGroundColor= "Red" },
+                new Materia {NomeMateria="Programação Orientada a Objetos",Id=1,BackGroundColor= "Red" },
+                 new Materia {NomeMateria="Programação Orientada a Objetos",Id=1,BackGroundColor= "Red" },
+                  new Materia {NomeMateria="Programação Orientada a Objetos",Id=1,BackGroundColor= "Red" },
+                   new Materia {NomeMateria="Programação Orientada a Objetos",Id=1,BackGroundColor= "Red" },
+                   new Materia {NomeMateria="Programação Orientada a Objetos",Id=1,BackGroundColor= "Red" },
+                   new Materia {NomeMateria="Programação Orientada a Objetos",Id=1,BackGroundColor= "Red" },
+    };
+    public MateriaPage()
+    {
+        InitializeComponent();
+
+        MateriaCollection.ItemsSource = listMateria;
+    }
 }
