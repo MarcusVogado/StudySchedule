68c7c6908501fb459003c638db9058dcda1402ee
marcus vogado <marcus_620@hotmail.com>
2023-05-18 23:51:20 -0300

    update timeSpan add

--- StudySchedule/Pages/StartStudyPage.xaml.cs
+++ StudySchedule/Pages/StartStudyPage.xaml.cs
@@ -1,9 +1,12 @@
 using MigrationLibrary.Models;
+using System.Reflection;
+
+
 namespace StudySchedule.Pages;
 
 public partial class StartStudyPage : ContentPage
 {
-   TimeSpan timer ;
+    TimeSpan timer;
     int horas;
     int minutos;
     [Obsolete]
@@ -23,7 +26,15 @@ public partial class StartStudyPage : ContentPage
 
     private void playStop_Clicked(object sender, EventArgs e)
     {
-
+        Device.StartTimer(new TimeSpan(horas, minutos, 60), () =>
+        {
+            // do something every 60 seconds
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                // interact with UI elements
+            });
+            return true; // runs again, or false to stop
+        });
 
     }
 
@@ -40,12 +51,35 @@ public partial class StartStudyPage : ContentPage
 
     private void ItemAlteradoScrolled(object sender, CurrentItemChangedEventArgs e)
     {
-        var itemSelected= (CurrentItemChangedEventArgs)e;
-        var agenda= (Agenda)itemSelected.CurrentItem;
-        int horas = agenda.Duracao / 60;
-        int minutos = agenda.Duracao % 60;
-        timer = new TimeSpan(horas,minutos,59) ;
+        var itemSelected = (CurrentItemChangedEventArgs)e;
+        Agenda agenda = new Agenda();
+        if (itemSelected.CurrentItem != null)
+        {
+            var propriedades = getPropertyValues(itemSelected.CurrentItem);
+            var duracao = propriedades.Where(a => a.Key == "Agenda");
+            foreach (var a in duracao)
+            {
+                var agendaValue = a.Value;
+                agenda = (Agenda)agendaValue;
+            }
+            horas = agenda.Duracao / 60;
+            minutos = agenda.Duracao % 60;
+            timer = new TimeSpan(horas, minutos, 59);
+            timeItemSelected.Text = timer.ToString();
+        }
+        return;
 
-        timeItemSelected.Text=timer.ToString();
+    }
+    public static Dictionary<string, object> getPropertyValues(object o)
+    {
+        if(o == null) return null;
+        Dictionary<string, object> propertyValues = new Dictionary<string, object>();
+        Type ObjectType = o.GetType();
+        System.Reflection.PropertyInfo[] properties = ObjectType.GetProperties();
+        foreach (System.Reflection.PropertyInfo property in properties)
+        {
+            propertyValues.Add(property.Name, property.GetValue(o, null));
+        }
+        return propertyValues;
     }
 }
