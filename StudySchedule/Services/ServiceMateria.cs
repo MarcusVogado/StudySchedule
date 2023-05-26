905c78f31e33aa8fb832869bc442bc0e22c15f7c
marcus vogado <marcus_620@hotmail.com>
2023-05-25 23:03:30 -0300

    create CRUD for Materia services

--- StudySchedule/Services/ServiceMateria.cs
+++ StudySchedule/Services/ServiceMateria.cs
@@ -1,4 +1,5 @@
-﻿using MigrationLibrary.Models;
+﻿using MigrationLibrary;
+using MigrationLibrary.Models;
 using StudySchedule.Contracts;
 using System;
 using System.Collections.Generic;
@@ -10,13 +11,35 @@ namespace StudySchedule.Services
 {
     public class ServiceMateria : IServiceMateria
     {
+        private readonly DbStudyContext _dbStudyContext;
+
+        public ServiceMateria(DbStudyContext dbStudyContext)
+        {
+            _dbStudyContext = dbStudyContext;
+        }
         public bool Create(Materia materia)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (materia == null) return false;
+                _dbStudyContext.Add(materia);
+                var confirm = _dbStudyContext.SaveChanges();
+                return (confirm > 0) ? true : false;
+
+            }
+            catch
+            {
+                return false;
+            }
         }
         public Materia Get(Materia materiaId)
         {
-            throw new NotImplementedException();
+            var materia = _dbStudyContext.Materias.FirstOrDefault(materiaId);
+            if (materia != null)
+            {
+                return materia;
+            }
+            return null;    
         }
         public ICollection<Materia> GetMaterias(Materia materiaId)
         {
